5798fad08c003b08e5b2e445fd2ccdcd6a2d19f7
Qamar Ashfaq <qamarashfaq41@gmail.com>
2020-07-17 17:32:01 +0500

    react app done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './hello.js';
 
 function App() {
   return (
+
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div >
+    <h1 className='my'>Hello Qamar</h1>
+    <p>Python Django Developer</p>
+    <img src='https://avatars2.githubusercontent.com/u/52790165?s=460&u=e1099db810cf0118780fa9b50e498bd711a891a8&v=4'></img>
     </div>
+      </header>
+
+<Hello skills='Web Design , Web Development ' tools='Git , Linux , WordPress'  />
+
+<hr/>
+
+<Hello skills='Web Design , Web Development ' tools='Git , Linux , WordPress'  />
+
+<hr/>
+<Hello skills='Web Design , Web Development ' tools='Git , Linux , WordPress'  />
+    </div>
+    
   );
 }
 
---
+++ src/hello.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Hello(props){
+
+    return (
+
+        <div>
+    <h2>My Skill is {props.skills}</h2>
+    <h2>My Skill is {props.tools}</h2>
+    </div>
+    )
+
+
+}
+export default Hello
